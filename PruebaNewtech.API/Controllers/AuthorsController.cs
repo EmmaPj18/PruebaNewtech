05c20f92a6b811a616ea2c9f46858b57dfa50de0
Emmanuel Polanco Jimenez <emmapj18@gmail.com>
2020-09-07 23:27:54 -0400

    Add project files.

---
+++ PruebaNewtech.API/Controllers/AuthorsController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaNewtech.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        public HttpClient Client { get; }
+
+        public AuthorsController(HttpClient Client)
+        {
+            this.Client = Client;
+        }
+    }
+}
---
+++ PruebaNewtech.API/Controllers/BooksController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PruebaNewtech.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        public HttpClient Client { get; }
+
+        public BooksController(HttpClient Client)
+        {
+            this.Client = Client;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            return Ok();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get()
+        {
+            return Ok();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create()
+        {
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Edit()
+        {
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete()
+        {
+            return Ok();
+        }
+    }
+}
